6b8f6a8f3fb0f6a4e176771e4c4abfb098e2e13d
Preston Brubaker <130982345+prestonbrubaker@users.noreply.github.com>
2024-02-13 21:12:48 -0600

    Update generator.py

--- generator.py
+++ generator.py
@@ -3,14 +3,75 @@ from torchvision import transforms
 from PIL import Image
 import os
 
-# Assuming the VariationalAutoencoder class is defined in vae_model.py
-from vae_model import VariationalAutoencoder
+class VariationalAutoencoder(nn.Module):
+    def __init__(self, latent_dim):
+        super(VariationalAutoencoder, self).__init__()
+        self.latent_dim = latent_dim
+
+       # Encoder
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=4, stride=2, padding=1),  # Output: 16x128x128
+            nn.ReLU(),
+            nn.Conv2d(16, 32, kernel_size=4, stride=2, padding=1),  # Output: 64x64x64
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),  # Output: 128x32x32
+            nn.ReLU(),
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),  # Output: 256x16x16
+            nn.ReLU(),
+            nn.Flatten(),  # Flatten for linear layer input
+            nn.Linear(128*16*16, 1024),
+            nn.ReLU()
+        )
+
+        self.fc_mu = nn.Linear(1024, latent_dim)
+        self.fc_log_var = nn.Linear(1024, latent_dim)
+
+        # Decoder
+        self.decoder_input = nn.Linear(latent_dim, 1024)
+
+        self.decoder = nn.Sequential(
+            nn.Linear(1024, 128*16*16),
+            nn.ReLU(),
+            nn.Unflatten(1, (128, 16, 16)),  # Unflatten to 256x16x16 for conv transpose input
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),  # Output: 128x32x32
+            nn.ReLU(),
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),  # Output: 64x64x64
+            nn.ReLU(),
+            nn.ConvTranspose2d(32, 16, kernel_size=4, stride=2, padding=1),  # Output: 32x128x128
+            nn.ReLU(),
+            nn.ConvTranspose2d(16, 1, kernel_size=4, stride=2, padding=1),  # Output: 1x256x256
+            nn.Sigmoid()
+        )
+
+    def encode(self, x):
+        x = self.encoder(x)
+        mu = self.fc_mu(x)
+        log_var = self.fc_log_var(x)
+        return mu, log_var
+
+    def reparameterize(self, mu, log_var):
+        std = torch.exp(0.5 * log_var)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+
+    def decode(self, z):
+        x = self.decoder_input(z)
+        x = self.decoder(x)
+        return x
+
+    def forward(self, x):
+        mu, log_var = self.encode(x)
+        z = self.reparameterize(mu, log_var)
+        return self.decode(z), mu, log_var
+
 
 # Model Parameters
 latent_dim = 256  # Example latent space dimension
 
 # Device Configuration
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using: " + str(device))
+
 
 def load_model(path, latent_dim, device):
     """Load the trained VAE model from a given path."""
